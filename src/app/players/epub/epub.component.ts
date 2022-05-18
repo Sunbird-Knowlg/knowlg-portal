da7e20c4e70854149639fc49bf62a24b38ae1e24
Gourav More <gourav_m@tekditechnologies.com>
2022-05-18 13:19:20 +0530

    Issue #SB-29849 fix: PR review comments and config changes

--- src/app/players/epub/epub.component.ts
+++ src/app/players/epub/epub.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 import { delay, first, mergeMap, tap } from 'rxjs/operators';
 import { HelperService } from 'src/app/services/helper/helper.service';
+import { ConfigService } from 'src/app/services/config/config.service';
 
 @Component({
   selector: 'app-epub',
@@ -11,97 +12,14 @@ import { HelperService } from 'src/app/services/helper/helper.service';
 })
 export class EpubComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute, private helperService: HelperService) { }
-  mode = '';
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private helperService: HelperService,
+    private configService: ConfigService
+  ) { }
   public queryParams: any;
   public contentDetails: any;
-  playerConfig = {
-    context: {
-      mode: 'play',
-      authToken: '',
-      sid: '7283cf2e-d215-9944-b0c5-269489c6fa56',
-      did: '3c0a3724311fe944dec5df559cc4e006',
-      uid: 'anonymous',
-      channel: '505c7c48ac6dc1edc9b08f21db5a571d',
-      pdata: {
-        id: 'sunbird.portal',
-        ver: '3.2.12',
-        pid: 'sunbird-portal.contentplayer'
-      },
-      contextRollup: {
-        l1: '505c7c48ac6dc1edc9b08f21db5a571d'
-      },
-      tags: [
-        ''
-      ],
-      cdata: [],
-      timeDiff: 0,
-      objectRollup: {},
-      host: '',
-      endpoint: '',
-      userData: {
-        firstName: 'Sourav',
-        lastName: 'Dey'
-      }
-    },
-    config: {
-      traceId: '123456',
-      sideMenu: {
-        showShare: true,
-        showDownload: true,
-        showReplay: false,
-        showExit: false,
-      }
-    },
-    metadata: {
-      compatibilityLevel: 4,
-      copyright: 'Kendriya_Vidyalaya,2020',
-      keywords: ['epub'],
-      subject: ['Science'],
-      language: ['English'],
-      mimeType: 'application/epub',
-      objectType: 'Content',
-      gradeLevel: ['Class 10'],
-      appIcon: 'https://preprodall.blob.core.windows.net/ntp-content-preprod/content/do_2130944360583331841866/artifact/book.jpg',
-      primaryCategory: 'Course Assessment',
-      // tslint:disable-next-line:max-line-length
-      artifactUrl: 'https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/content/assets/do_2132118800055091201207/kehs102.epub',
-      contentType: 'SelfAssess',
-      identifier: 'do_21312960731822489612047',
-      audience: ['Student'],
-      visibility: 'Default',
-      mediaType: 'content',
-      osId: 'org.ekstep.quiz.app',
-      languageCode: ['en'],
-      license: 'CC BY 4.0',
-      name: 'EPUB_229.epub',
-      attributions: ['kanmani'],
-      status: 'Live',
-      code: '49f3ea6d-db45-a4a7-fcd6-daf58785c7db',
-      description: 'epub',
-      // tslint:disable-next-line:max-line-length
-      streamingUrl: 'https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/content/do_21312960731822489612047/artifact/index.epub',
-      medium: ['English'],
-      createdOn: '2020-10-15T04:14:28.339+0000',
-      lastUpdatedOn: '2020-10-15T04:19:30.101+0000',
-      originData: {
-        identifier: 'do_21312917869280460814945',
-        repository: 'https://dock.preprod.ntp.net.in/api/content/v1/read/do_21312917869280460814945'
-      },
-      creator: 'classmate2',
-      pkgVersion: 1,
-      versionKey: '1602735570101',
-      createdBy: '7ff59e4d-0f4f-4f25-8244-969423e91a16',
-      board: 'CBSE',
-      resourceType: 'Learn',
-      orgDetails: {},
-      licenseDetails: {
-        name: 'CC BY 4.0',
-        url: 'https://creativecommons.org/licenses/by/4.0/legalcode',
-        description: 'For details see below:'
-      }
-    }
-  };
+  playerConfig = this.configService.playerConfig.EPUB_PLAYER;
 
   ngOnInit(): void {
     this.queryParams = this.activatedRoute.snapshot.queryParams;
@@ -120,9 +38,6 @@ export class EpubComponent implements OnInit {
           console.log('error --->', error);
         }
       );
-    this.activatedRoute.queryParams.subscribe(params => {
-        this.mode = params.mode;
-    });
   }
 
   private getContentDetails() {
@@ -145,14 +60,6 @@ export class EpubComponent implements OnInit {
     setTimeout(() => {
       this.playerConfig = contentDetails;
     }, 3000);
-  }
-
-  setArtifactUrl(artifactUrl: string){
-    const metadata = this.playerConfig.metadata;
-    metadata.streamingUrl = artifactUrl;
-    const config = this.playerConfig;
-    this.contentDetails = {...config, metadata};
-    this.loadContent(this.contentDetails);
   }
 
   playerEvents(event) {
--- src/app/players/pdf/pdf.component.ts
+++ src/app/players/pdf/pdf.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 import { delay, first, mergeMap, tap } from 'rxjs/operators';
 import { HelperService } from 'src/app/services/helper/helper.service';
+import { ConfigService } from 'src/app/services/config/config.service';
 
 @Component({
   selector: 'app-pdf',
@@ -10,74 +11,15 @@ import { HelperService } from 'src/app/services/helper/helper.service';
   styleUrls: ['./pdf.component.scss']
 })
 export class PdfComponent implements OnInit {
-  constructor(private activatedRoute: ActivatedRoute, private helperService: HelperService) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private helperService: HelperService,
+    private configService: ConfigService
+  ) { }
   @ViewChild('preview', { static: false }) previewElement: ElementRef;
-  mode = '';
   public queryParams: any;
   public contentDetails: any;
-  playerConfig = {
-    context: {
-      mode: 'play',
-      authToken: '',
-      sid: '7283cf2e-d215-9944-b0c5-269489c6fa56',
-      did: '3c0a3724311fe944dec5df559cc4e006',
-      uid: 'anonymous',
-      channel: '505c7c48ac6dc1edc9b08f21db5a571d',
-      pdata: {
-        id: 'sunbird.portal',
-        ver: '3.2.12',
-        pid: 'sunbird-portal.contentplayer'
-      },
-      contextRollup: {
-        l1: '505c7c48ac6dc1edc9b08f21db5a571d'
-      },
-      tags: [
-        ''
-      ],
-      cdata: [],
-      timeDiff: 0,
-      objectRollup: {},
-      host: '',
-      endpoint: '',
-      userData: {
-        firstName: 'Sourav',
-        lastName: 'Dey'
-      }
-    },
-    config: {},
-    metadata: {
-      compatibilityLevel: 4,
-      artifactUrl: 'https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/assets/do_31291455031832576019477/b301b302_std_1_tamilenglish_lang_term-1_opt.pdf',
-      contentType: 'FocusSpot',
-      identifier: 'do_31291455031832576019477',
-      audience: ['Teacher'],
-      visibility: 'Default',
-      mediaType: 'content',
-      osId: 'org.ekstep.quiz.app',
-      languageCode: ['en'],
-      license: 'CC BY 4.0',
-      name: 'B301,ENGLISH_LANG_TERM 1_OPT',
-      status: 'Live',
-      code: '43e68089-997e-49a4-902a-6262e5654515',
-      description: 'epdf',
-      streamingUrl: 'https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/assets/do_31291458881611366418883/b331332333_std_5_mathssciencesocial_tm_term-1_opt.pdf',
-      createdOn: '2019-12-16T07:59:53.154+0000',
-      copyrightYear: 2019,
-      additionalCategories: ['Focus Spot'],
-      lastUpdatedOn: '2019-12-16T11:52:56.405+0000',
-      creator: 'SCERT 2 ECONTENTS',
-      pkgVersion: 1,
-      versionKey: '1576497176405',
-      framework: 'tn_k-12_5',
-      createdBy: 'f4f80b17-8609-44b9-b781-b79df5cf7e8d',
-      resourceType: 'Read',
-      licenseDetails: {
-        name: 'CC BY 4.0',
-        url: 'https://creativecommons.org/licenses/by/4.0/legalcode',
-        description: 'For details see below:'
-      }
-    },
-  };
+  playerConfig = this.configService.playerConfig.PDF_PLAYER;
 
   ngOnInit(): void {
     this.queryParams = this.activatedRoute.snapshot.queryParams;
@@ -96,9 +38,6 @@ export class PdfComponent implements OnInit {
           console.log('error --->', error);
         }
       );
-    this.activatedRoute.queryParams.subscribe(params => {
-        this.mode = params.mode;
-    });
   }
 
   private getContentDetails() {
@@ -122,12 +61,6 @@ export class PdfComponent implements OnInit {
     setTimeout(() => {
       this.playerConfig = {...config, metadata};
     }, 3000);
-  }
-
-  setArtifactUrl(artifactUrl: string){
-    const metadata = this.playerConfig.metadata;
-    metadata.streamingUrl = artifactUrl;
-    this.loadContent(metadata);
   }
 
   playerEvents(event) {
