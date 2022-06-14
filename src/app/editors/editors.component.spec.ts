31721a7ec58be41268f1d684bf51c7d24200718f
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2022-06-14 21:42:22 +0530

    Issue #SB-30124 fix: test-cases issue fixes

--- src/app/editors/editors.component.spec.ts
+++ src/app/editors/editors.component.spec.ts
@@ -31,20 +31,7 @@ describe('EditorsComponent', () => {
   it('#navigateToContentList should navigate to "collection-editor" ', () => {
     const router = TestBed.inject(Router);
     component.navigateToContentList('collection-editor');
-    expect(router.navigate).toHaveBeenCalledWith(['editors/content-list/collection-editor']);
+    expect(router.navigate).toHaveBeenCalledWith(['editors/content-list']);
   });
-
-  it('#navigateToFileUploadEditor should navigate to "fileUploadEditor" ', () => {
-    const router = TestBed.inject(Router);
-    component.navigateToFileUploadEditor('do_123456789');
-    expect(router.navigate).toHaveBeenCalledWith(['editors/file-upload-editor'], { queryParams: { identifier: 'do_123456789' } });
-  });
-
-  it('#navigateToInteractiveEditor should navigate to "InteractiveEditor" ', () => {
-    const router = TestBed.inject(Router);
-    component.navigateToInteractiveEditor('do_123456789');
-    expect(router.navigate).toHaveBeenCalledWith(['editors/interactive-editor'], { queryParams: { identifier: 'do_123456789' } });
-  });
-
 
 });
