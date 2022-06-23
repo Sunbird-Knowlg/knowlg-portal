00d24f7e9054975bf3031efab19bba37ecf36cfe
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2022-06-23 16:50:12 +0530

    #SB-30225 feat: Enable User selection functionality

--- src/app/editors/role/role.component.ts
+++ src/app/editors/role/role.component.ts
@@ -1,54 +1,50 @@
 
 
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from '../../services/user/localstorage.service';
 import { HelperService } from '../../services/helper/helper.service';
 import * as _ from 'lodash-es';
-import { Router } from '@angular/router';
-import {MatDialog} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { UserComponent } from '../user/user.component';
+import { ConfigService } from '../../services/config/config.service';
 @Component({
   selector: 'app-role',
   templateUrl: './role.component.html',
   styleUrls: ['./role.component.scss']
 })
 export class RoleComponent implements OnInit {
-rolesArray = [];
+  rolesArray = [];
   userArray = [];
   selectedRoleType: any;
   constructor(public localStorageService: LocalStorageService,
-              private  dialog: MatDialog,
-              public helperService: HelperService, private router: Router) { }
+              private dialog: MatDialog,
+              public configService: ConfigService,
+              public helperService: HelperService) { }
   ngOnInit(): void {
     this.getAllRoleTypes();
   }
   getAllRoleTypes() {
-    this.helperService.getAllRoleTypes()
-      .subscribe((response) => {
-        this.rolesArray = response;
-      }, (error) => {
-        this.rolesArray = error;
-        console.log(error);
-      });
+    this.helperService.getAllRoleTypes().subscribe((response) => {
+      this.rolesArray = _.get(response, 'result.roles');
+    }, (error) => {
+      console.log(error);
+    });
   }
-  getAllUsersByRoletype(roleType) {
+  getAllUsersByRoleType(roleType) {
     this.selectedRoleType = roleType;
-    this.helperService.getAllUsersByRoleType(roleType)
-      .subscribe((response) => {
-        this.userArray = response;
-        this.openDialog(this.userArray);
-      }, (error) => {
-        this.userArray = error;
-        this.openDialog(this.userArray);
-        console.log(error);
-      });
+    this.helperService.getAllUsersByRoleType(roleType).subscribe((response) => {
+      this.userArray = _.get(response, 'result.users');
+      this.openDialog(this.userArray);
+    }, (error) => {
+      console.log(error);
+    });
   }
   openDialog(userData): void {
-   this.dialog.open(UserComponent, {
+    this.dialog.open(UserComponent, {
       data: {
         userArray: userData,
         selectedRoleType: this.selectedRoleType
       }
     });
-}
+  }
 }
