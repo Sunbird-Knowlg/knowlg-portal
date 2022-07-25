9465d3c156f232e966ab19c0f22247c604c295a6
Harish Kumar Gangula <harish@sanketika.in>
2022-07-25 15:46:47 +0530

    Issue SB-30413 fix: lint fix

--- src/app/services/player/player.service.ts
+++ src/app/services/player/player.service.ts
@@ -18,9 +18,9 @@ export class PlayerService {
     private configService: ConfigService,
     private activatedRoute: ActivatedRoute
     ) {
-    this.defaultContentDataMap["do_11348995249825382411"] = this.configService.playerConfig.PDF_PLAYER_METADATA;
-    this.defaultContentDataMap["do_21312960731822489612047"] = this.configService.playerConfig.EPUB_PLAYER_METADATA;
-    this.defaultContentDataMap["do_31309320735055872011111"] = this.configService.playerConfig.VIDEO_PLAYER_METADATA;
+    this.defaultContentDataMap['do_11348995249825382411'] = this.configService.playerConfig.PDF_PLAYER_METADATA;
+    this.defaultContentDataMap['do_21312960731822489612047'] = this.configService.playerConfig.EPUB_PLAYER_METADATA;
+    this.defaultContentDataMap['do_31309320735055872011111'] = this.configService.playerConfig.VIDEO_PLAYER_METADATA;
     this.identifier$ = new Subject<string>();
     this.contentChangeObservable = this.identifier$.asObservable();
     this.activatedRoute.paramMap.subscribe(params => {
