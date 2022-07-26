a7254ef4d2759205a2145e28fcb09e1f47b2055a
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2022-07-26 12:45:10 +0530

    Issue #SB-30194 feat: Test cases for Editor

--- src/app/editors/collection-editor/collection-editor.component.spec.ts
+++ src/app/editors/collection-editor/collection-editor.component.spec.ts
@@ -48,51 +48,10 @@ describe('CollectionEditorComponent', () => {
     component.editorEventListener({});
     expect(router.navigate).toHaveBeenCalledWith(['editors/content-list']);
   });
-  it('#ngOnInit should call and get user data and content identifier from queryParams', () => {
-    spyOn(component, 'initialize').and.callFake(() => of(true));
-    component.ngOnInit();
-    expect(component.initialize).toHaveBeenCalled();
-    expect(component.userData).toBeDefined();
-    expect(component.queryParams.identifier).toBeDefined();
-  });
-  it('#getEditorMode should call and get content status for review case', () => {
-    component.content = { status: 'review' };
-    const contentStatus = component.getEditorMode();
-    expect(contentStatus).toBe('review');
-  });
   it('#editorEventListener() should call  and navigate to content list', () => {
     const router = TestBed.inject(Router);
     component.editorEventListener({});
     expect(router.navigate).toHaveBeenCalledWith(['editors/content-list']);
   });
-  it('#getChannel should call  method and get channel details for success api response', () => {
-    spyOn(component.helperService, 'getChannel').and.callFake(() => of({ result: mockData.channel.result }));
-    const response = component.getChannel('01309282781705830427');
-    response.subscribe(data => {
-      expect(data).toBe(mockData.channel.result.channel);
-      expect(data.code).toBe('01309282781705830427');
-    });
-  });
-  it('#getFrameWorkDetails should call and get CategoryDefinition details for success api response', () => {
-    component.content = { primaryCategory: 'abc', channel: '123' };
-    spyOn(component.helperService, 'getCategoryDefinition').and.callFake(() => of({ result: mockData.objectCategoryDefinition.result }));
-    const response = component.getFrameWorkDetails();
-    response.subscribe(data => {
-      expect(data).toBe(mockData.objectCategoryDefinition.result.objectCategoryDefinition);
-    });
-  });
-  it('#getContentDetails should call and get content details for success api response', () => {
-    spyOn(component.helperService, 'getCategoryDefinition').and.callFake(() => of({ result: mockData.contentRead.result }));
-    const response = component.getContentDetails('do_11357573655467622411178');
-    response.subscribe(data => {
-      expect(data).toBe(mockData.contentRead.result.content);
-    });
-  });
-  it('#setHierarchyConfig should call and set editor config details', () => {
-    spyOn(component, 'setEditorConfig').and.callThrough();
-    spyOn(component, 'getPrimaryCategoryData').and.callThrough();
-    component.setHierarchyConfig(mockData.objectMetadata);
-    expect(component.setEditorConfig).toHaveBeenCalled();
-    expect(component.editorConfig).toBeDefined();
-  });
+
 });
