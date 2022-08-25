31b2f2c2cadcc5848adb535f9b0b26bd450a5c5f
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2022-08-25 12:36:14 +0530

    Issue #KN-239 feat: Knowlg player app for PDF, Video and Epub player

---
+++ knowlg-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'knowlg-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
