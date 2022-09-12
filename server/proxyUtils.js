7069ffc75b347fb0dd727d81a7c712d4ba1a613b
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2022-09-12 11:41:43 +0530

    Issue #KN-0000 fix: create lock API issue fixes

--- server/proxyUtils.js
+++ server/proxyUtils.js
@@ -1,17 +1,21 @@
 const envVariables = require('./config/environment');
-var publicRequestHeaders = {
+const config = require('./config/constants');
+const publicRequestHeaders = {
   authorization: `Bearer ${envVariables.API_AUTH_TOKEN}`,
   "x-authenticated-user-token": envVariables.USER_API_TOKEN,
-  "x-channel-id": envVariables.CHANNEL_ID,
-  'x-authenticated-userid': "5a587cc1-e018-4859-a0a8-e842650b9d64" // TODO:: added temp for lock API and have to make more dynamic (use header user-id and remove this)
+  "x-channel-id": envVariables.CHANNEL_ID
 };
 
-var contentTypeHeaders = {
+const contentTypeHeaders = {
   'content-type': "application/json"
 }
 
 const decoratePublicRequestHeaders = function () {
-  return function (proxyReqOpts, srcReq) {      
+  return function (proxyReqOpts, srcReq) {  
+      if(srcReq.get('user-id')) {
+        const userId = srcReq.get('user-id') === 'content-editor' ? config.defaultUserId : srcReq.get('user-id'); // TODO:: added this condition temp for lock API and have to make more dynamic (use header user-id and remove this condition) 
+        proxyReqOpts.headers['x-authenticated-userid'] = userId;
+      }    
       proxyReqOpts.headers = Object.assign({}, proxyReqOpts.headers, publicRequestHeaders, contentTypeHeaders);
       return proxyReqOpts;
   }
