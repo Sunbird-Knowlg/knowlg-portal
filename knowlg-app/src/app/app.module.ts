72afc538094e6097ef4fd7b7d2b9e70fdf51a6dd
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2022-09-12 12:08:19 +0530

    Issue #KN-239 feat: Knowlg player app for EPUB player

--- knowlg-app/src/app/app.module.ts
+++ knowlg-app/src/app/app.module.ts
@@ -7,9 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SunbirdPdfPlayerModule } from '@project-sunbird/sunbird-pdf-player-v9';
+import { SunbirdEpubPlayerModule } from '@project-sunbird/sunbird-epub-player-v9';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SunbirdPdfPlayerModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SunbirdPdfPlayerModule, SunbirdEpubPlayerModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- projects/shared/src/lib/shared.module.ts
+++ projects/shared/src/lib/shared.module.ts
@@ -4,9 +4,10 @@ import { SunbirdPdfPlayerModule } from '@project-sunbird/sunbird-pdf-player-v9';
 import { PlayerComponent } from './player/player.component';
 import { CommonModule } from '@angular/common';
 import { ContentlistComponent } from './contentlist/contentlist.component';
+import { SunbirdEpubPlayerModule } from '@project-sunbird/sunbird-epub-player-v9';
 @NgModule({
   declarations: [SharedComponent, PlayerComponent, ContentlistComponent],
-  imports: [ SunbirdPdfPlayerModule, CommonModule
+  imports: [ SunbirdPdfPlayerModule, SunbirdEpubPlayerModule, CommonModule
   ],
   exports: [SharedComponent, PlayerComponent, ContentlistComponent]
 })
