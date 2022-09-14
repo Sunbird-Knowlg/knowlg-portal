21e7e175e3ede205b1f73ad6d048e5bb4614f5ba
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2022-09-14 14:28:52 +0530

    Issue #KN-239 feat: Knowlg player app for Video player

--- knowlg-app/src/app/home/services/data.ts
+++ knowlg-app/src/app/home/services/data.ts
@@ -339,7 +339,164 @@ export const  data = {
       ],
       video: [
         {
-          "interceptionPoints": "{'items':[{'type':'QuestionSet','interceptionPoint':50,'identifier':'do_213272808198291456121'},{'type':'QuestionSet','interceptionPoint':90,'identifier':'do_213272808198291456121'},{'type':'QuestionSet','interceptionPoint':120,'identifier':'do_213272808198291456121'}]}",
+          copyright: 'Kendriya_Vidyalaya,2020',
+          keywords: [
+            'mp4'
+          ],
+          subject: [
+            'Hindi'
+          ],
+          channel: 'diksha',
+          language: [
+            'English'
+          ],
+          mimeType: 'video/mp4',
+          objectType: 'Content',
+          gradeLevel: [
+            'Class 10'
+          ],
+          // tslint:disable-next-line
+          appIcon: 'https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/content/do_21310353608830976014671/artifact/content.thumb.jpg',
+          primaryCategory: 'Explanation Content',
+          // tslint:disable-next-line
+          artifactUrl: 'https://sunbirdstagingpublic.blob.core.windows.net/sunbird-content-staging/content/do_21310353608830976014671/artifact/mp4_14.mp4',
+          contentType: 'ExplanationResource',
+          identifier: 'do_21310353608830976014671',
+          audience: [
+            'Student'
+          ],
+          visibility: 'Default',
+          mediaType: 'content',
+          osId: 'org.ekstep.quiz.app',
+          languageCode: [
+            'en'
+          ],
+          license: 'CC BY 4.0',
+          name: 'mp4_14.mp4',
+          interceptionType: 'Timestamp',
+          attributions: [
+            'kanmani'
+          ],
+          status: 'Live',
+          code: '2a38c802-a986-cba4-3e74-f666b7983b01',
+          interceptionPoints: {
+            items: [
+              {
+                type: 'QuestionSet',
+                interceptionPoint: 50,
+                identifier: 'do_213487261245874176191'
+              },
+              {
+                type: 'QuestionSet',
+                interceptionPoint: 120,
+                identifier: 'do_213272808198291456121'
+              },
+              {
+                type: 'QuestionSet',
+                interceptionPoint: 80,
+                identifier: 'do_213471560301830144119'
+              }
+            ]
+          },
+          description: 'mp4',
+          // tslint:disable-next-line
+          streamingUrl: 'https://ntppreprodmedia-inct.streaming.media.azure.net/409780ae-3fc2-4879-85f7-f1affcce55fa/mp4_14.ism/manifest(format=m3u8-aapl-v3)',
+          medium: [
+            'English'
+          ],
+          createdOn: '2020-09-08T08:12:25.155+0000',
+          lastUpdatedOn: '2021-07-13T05:32:48.505+0000',
+          originData: {
+            identifier: 'do_21310017032908800011930',
+            repository: 'https://dock.preprod.ntp.net.in/api/content/v1/read/do_21310017032908800011930'
+          },
+          creator: 'kanmani1',
+          pkgVersion: 2,
+          versionKey: '1626154368569',
+          framework: 'ekstep_ncert_k-12',
+          createdBy: '95f436bd-aad2-498c-b982-c941ef44a06c',
+          board: 'CBSE',
+          resourceType: 'Learn',
+          orgDetails: {},
+          licenseDetails: {
+            name: 'CC BY 4.0',
+            url: 'https://creativecommons.org/licenses/by/4.0/legalcode',
+            description: 'For details see below:'
+          },
+          transcripts: [
+            {
+                language: 'English',
+                languageCode: 'en',
+                identifier: 'do_11355096882872320012900',
+                // tslint:disable-next-line:max-line-length
+                artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11355096882872320012900/caption.english.vtt'
+            },
+            {
+                language: 'Urdu',
+                languageCode: 'ur',
+                identifier: 'do_11355155567137587212904',
+                // tslint:disable-next-line:max-line-length
+                artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11355155567137587212904/caption.urdu.vtt'
+            },
+            {
+                language: 'Bengali',
+                languageCode: 'bn',
+                identifier: 'do_11355160347001651212907',
+                // tslint:disable-next-line:max-line-length
+                artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11355160347001651212907/caption.bengali.vtt'
+            },
+             {
+                language: 'Marathi',
+                languageCode: 'mr',
+                identifier: 'do_11355160521817292812908',
+                // tslint:disable-next-line:max-line-length
+                artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11355160521817292812908/caption.marathi.vtt'
+            },
+            {
+                language: 'Odiya',
+                languageCode: 'or',
+                identifier: 'do_11355160685255065612910',
+                // tslint:disable-next-line:max-line-length
+                artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11355160685255065612910/caption.odia.vtt'
+            },
+            {
+                language: 'Telugu',
+                languageCode: 'te',
+                identifier: 'do_11355160762735001612911',
+                // tslint:disable-next-line:max-line-length
+                artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11355160762735001612911/caption.telugu.vtt'
+            },
+            {
+                language: 'Tamil',
+                languageCode: 'ta',
+                identifier: 'do_11355160835553689612912',
+                // tslint:disable-next-line:max-line-length
+                artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11355160835553689612912/caption.tamil.vtt'
+            },
+            {
+                language: 'Punjabi',
+                languageCode: 'pa',
+                identifier: 'do_11355160955663155212913',
+                // tslint:disable-next-line:max-line-length
+                artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11355160955663155212913/caption.punjabi.vtt'
+            },
+            {
+                language: 'Kannada',
+                languageCode: 'ka',
+                identifier: 'do_11355161075142656012914',
+                // tslint:disable-next-line:max-line-length
+                artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11355161075142656012914/caption.kannada.vtt'
+            },
+            {
+                language: 'Hindi',
+                languageCode: 'hi',
+                identifier: 'do_11355161161894297612915',
+                // tslint:disable-next-line:max-line-length
+                artifactUrl: 'https://dockstorage.blob.core.windows.net/sunbird-content-dock/content/assets/do_11355161161894297612915/caption.hindi.vtt'
+            }
+        ]
+        },
+        {
           "interceptionType": "Timestamp",
           "compatibilityLevel": 2,
           "copyright": "NCERT",
--- projects/shared/src/lib/shared.module.ts
+++ projects/shared/src/lib/shared.module.ts
@@ -5,14 +5,11 @@ import { PlayerComponent } from './player/player.component';
 import { CommonModule } from '@angular/common';
 import { ContentlistComponent } from './contentlist/contentlist.component';
 import { SunbirdEpubPlayerModule } from '@project-sunbird/sunbird-epub-player-v9';
-import { QuestionCursor } from '@project-sunbird/sunbird-quml-player-v9';
-import { QuestionCursorImplementationService } from './question-cursor-implementation.service';
 import { SunbirdVideoPlayerModule } from '@project-sunbird/sunbird-video-player-v9';
 @NgModule({
   declarations: [SharedComponent, ContentlistComponent, PlayerComponent],
   imports: [ SunbirdPdfPlayerModule, SunbirdEpubPlayerModule, SunbirdVideoPlayerModule, CommonModule
   ],
-  providers: [{ provide: QuestionCursor, useClass: QuestionCursorImplementationService }],
   exports: [SharedComponent, PlayerComponent, ContentlistComponent]
 })
 export class SharedModule { }
