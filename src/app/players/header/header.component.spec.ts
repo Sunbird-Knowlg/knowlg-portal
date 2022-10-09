caed711887bad8a5608a219da26d4d00cd4d9221
Vaibhav Bhuva <vaibhav.bhuva@tarento.com>
2022-10-09 16:20:43 +0530

    Issue #KN-38 fix: test-cases issue fixes

--- src/app/players/header/header.component.spec.ts
+++ src/app/players/header/header.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
 import { HeaderComponent } from './header.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { of } from 'rxjs';
+
+const mockActivatedRoute = {
+  queryParams: of({
+    identifier: 'do_21247940906829414411032',
+    mode: 'publish'
+  })
+};
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +18,12 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports: [MatDialogModule],
+      providers: [
+        MatDialog,
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
+      ],
     })
     .compileComponents();
   });
