d9f68294427314b4c2cc15df9e468085fc3b408c
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2022-10-19 16:55:18 +0530

    Issue #KN-316 feat: Pdf player: web component implementation for knowlg app

--- projects/shared/src/lib/shared.module.ts
+++ projects/shared/src/lib/shared.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { SharedComponent } from './shared.component';
-import { SunbirdPdfPlayerModule } from '@project-sunbird/sunbird-pdf-player-v9';
 import { PlayerComponent } from './player/player.component';
 import { CommonModule } from '@angular/common';
 import { ContentlistComponent } from './contentlist/contentlist.component';
 import { SunbirdEpubPlayerModule } from '@project-sunbird/sunbird-epub-player-v9';
 import { SunbirdVideoPlayerModule } from '@project-sunbird/sunbird-video-player-v9';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @NgModule({
   declarations: [SharedComponent, ContentlistComponent, PlayerComponent],
-  imports: [ SunbirdPdfPlayerModule, SunbirdEpubPlayerModule, SunbirdVideoPlayerModule, CommonModule
+  imports: [ SunbirdEpubPlayerModule, SunbirdVideoPlayerModule, CommonModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [SharedComponent, PlayerComponent, ContentlistComponent]
 })
 export class SharedModule { }
