336f10836c237572a094256647fb81ce2c82918b
Harish Kumar Gangula <harish@sanketika.in>
2022-10-25 10:15:51 +0530

    Issue #KN-535 feat: Removed unused components

--- projects/shared/src/lib/shared.module.ts
+++ projects/shared/src/lib/shared.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
-import { SharedComponent } from './shared.component';
 import { SunbirdPdfPlayerModule } from '@project-sunbird/sunbird-pdf-player-v9';
 import { PlayerComponent } from './player/player.component';
 import { CommonModule } from '@angular/common';
 import { ContentlistComponent } from './contentlist/contentlist.component';
 import { SunbirdEpubPlayerModule } from '@project-sunbird/sunbird-epub-player-v9';
 import { SunbirdVideoPlayerModule } from '@project-sunbird/sunbird-video-player-v9';
+import { ContentService } from './services/content.service';
 @NgModule({
-  declarations: [SharedComponent, ContentlistComponent, PlayerComponent],
+  declarations: [ContentlistComponent, PlayerComponent],
   imports: [ SunbirdPdfPlayerModule, SunbirdEpubPlayerModule, SunbirdVideoPlayerModule, CommonModule
   ],
-  exports: [SharedComponent, PlayerComponent, ContentlistComponent]
+  exports: [PlayerComponent, ContentlistComponent, ContentService]
 })
 export class SharedModule { }
