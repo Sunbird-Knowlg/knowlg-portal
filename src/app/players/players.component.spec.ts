d3ea54ca3e61fe6ca1dadfe4877663545109f35a
niit <santosh.mattikoppa@ilimi.in>
2022-11-04 18:08:42 +0530

    Issue #KN-573 feat: V2 Players: Common code base implementation in knowlg portal

--- src/app/players/players.component.spec.ts
+++ src/app/players/players.component.spec.ts
@@ -4,6 +4,7 @@ import { PlayersComponent } from './players.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { PlayerService } from 'src/app/services/player/player.service';
 import { ConfigService } from 'src/app/services/config/config.service';
+import { of } from 'rxjs';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 class RouterStub {
@@ -11,13 +12,13 @@ class RouterStub {
 }
 const mockActivatedRoute = {
   snapshot: {
-      queryParams: {
-          id: 'do_21247940906829414411032',
-      },
-      params: {
-        mimeType: 'pdf',
-        id: '1234'
-      }
+    queryParams: {
+      id: 'do_21247940906829414411032',
+    },
+    params: {
+      mimeType: 'pdf',
+      id: '1234'
+    }
   }
 };
 describe('PlayersComponent', () => {
@@ -36,7 +37,7 @@ describe('PlayersComponent', () => {
       declarations: [PlayersComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
-        HttpClient , ConfigService,
+        HttpClient, ConfigService,
         { provide: Router, useClass: RouterStub },
         { provide: PlayerService, useValue: playerServiceMock },
         { provide: ActivatedRoute, useValue: mockActivatedRoute },
@@ -53,5 +54,28 @@ describe('PlayersComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
+  it('should call setConfig() method and define playerSettingconfig and sidemenuConfig', () => {
+    spyOn(component.configService, 'getConfigData').and.returnValue(
+      {
+        traceId: 'afhjgh',
+        sideMenu: {
+          showDownload: true,
+          showExit: true,
+          showPrint: true,
+          showReplay: true,
+          showShare: true,
+        }
+      })
+    component.setConfig();
+    expect(component.playerSettingconfig).toBeDefined();
+    expect(component.sidemenuConfig).toBeDefined();
+  });
+  it('should call ngOnInit and define configType and call setConfig method', () => {
+    component['activatedRoute'].snapshot.params.mimeType = 'pdf'; 
+    spyOn(component, 'getContentDetails').and.callFake(() => of({}))
+    spyOn(component, 'setConfig').and.callFake(() => of({}))
+    component.ngOnInit();
+    expect(component.setConfig).toHaveBeenCalled();
+    expect(component.configType).toBe('pdfConfig');
+  });
 });
