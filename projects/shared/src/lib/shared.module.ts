eaa9a50c4a6e6b50f3ea3fc04e61e6f8d685ebfe
Santosh Mattikoppa <santosh.mattikoppa@ilimi.in>
2022-11-08 18:51:48 +0530

    Issue #KN-517 feat: Epub player web component integration in knowlg portal and app

--- projects/shared/src/lib/shared.module.ts
+++ projects/shared/src/lib/shared.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { PlayerComponent } from './player/player.component';
 import { CommonModule } from '@angular/common';
 import { ContentlistComponent } from './contentlist/contentlist.component';
-import { SunbirdEpubPlayerModule } from '@project-sunbird/sunbird-epub-player-v9';
 import { SunbirdVideoPlayerModule } from '@project-sunbird/sunbird-video-player-v9';
 import { ContentService } from './services/content.service';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @NgModule({
   declarations: [ ContentlistComponent, PlayerComponent],
-  imports: [ SunbirdEpubPlayerModule, SunbirdVideoPlayerModule, CommonModule
+  imports: [SunbirdVideoPlayerModule, CommonModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   exports: [PlayerComponent, ContentlistComponent],
